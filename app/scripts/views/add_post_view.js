1f46789bc90c476498e794ab7ce603f4f28122da
John Crocker <johnniecrocker@yahoo.com>
2014-11-11 15:00:51 -0500

    added initial script to add

--- app/scripts/views/add_post_view.js
+++ app/scripts/views/add_post_view.js
@@ -1,6 +1,38 @@
 
 (function(){
 
+  App.Views.AddPost = Parse.View.extend({
 
+    events: {
+      'submit #addpost' : 'addpost'
+    },
+
+    initialize: function(){
+      this.render();
+
+      $('#blogposts').html();
+
+    },
+
+    render: function(){
+      this.$el.html($('#addpost').html());
+
+    },
+
+    addpost: function(e){
+      e.preventDefault();
+
+      var post = new App.Models.Post({
+        title: $('#newusername').val(),
+        content: $('#blogcontent').val(),
+        tags: $('newtags').val(),
+        //add user
+        //add time and date stamp
+      });
+
+    }
+
+
+  });
 
 }());
