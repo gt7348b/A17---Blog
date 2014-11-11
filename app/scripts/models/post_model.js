14701dac53014c65a50c1bc4ac0b94e9c81a7526
John Crocker <johnniecrocker@yahoo.com>
2014-11-11 14:27:27 -0500

    initial model seems to be able to be created

--- app/scripts/models/post_model.js
+++ app/scripts/models/post_model.js
@@ -1,6 +1,26 @@
 
 (function(){
 
+  App.Models.Post = Parse.Object.extend({
+
+    className: 'blogPost',
+
+    defaults: {
+      title: '',
+      content: '',
+      tags: '',
+      user: '',
+      submitted: ''
+    },
+
+    idAttribute: 'objectID',
+
+    initialize: function(){
+      console.log("Lucky?");
+
+    }
+
+  });
 
 
 }());
