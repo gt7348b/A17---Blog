f4376a1a156109c64538768570433c3382619373
John Crocker <johnniecrocker@yahoo.com>
2014-11-11 13:46:25 -0500

    added parse initialize and empty functions to all js files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('The Iron Yard Rocks');
+//Initialize Parse
+
+Parse.initialize("wF5Pd5fI6w6c5jbKHdEM9qKg3lLaQAw7phwYLnz2", "aKAGgKJ26LBBhqksgQzCW4iFvTZHyEL1125Gwznr");
+
+//Work
+
+(function(){
+
+
+
+}());
