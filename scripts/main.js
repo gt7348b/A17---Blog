177703c2494018473a68cbfe88d635db29442c4a
Joanna Edgell <joedgell2013@gmail.com>
2014-11-15 12:54:44 -0500

    Author search

--- scripts/main.js
+++ scripts/main.js
@@ -325,7 +325,7 @@ App.Views.AuthorPost = Parse.View.extend ({
 
     },
 
-    template: _.template($('#mainblog').html()),
+    template: _.template($('#publicblog').html()),
 
   initialize: function(options) {
 
@@ -334,32 +334,12 @@ App.Views.AuthorPost = Parse.View.extend ({
     //this.collection.off();
     //this.collection.on('sync', this.render, this);
 
-    this.authorQuery();
+    this.render();
 
     $('#listBlogs').html(this.$el);
     console.log(this.collection);
   },
 
-  authorQuery: function(){
-
-    var self= this;
-
-    var author = new Parse.Query(App.Models.Post);
-
-    author.equalTo('user', App.user.attributes.username);
-
-    author.find({
-
-      success: function(results){
-
-        self.collection.models = results;
-
-        self.render();
-      }
-
-    });
-
-  },
 
   render: function(){
 
@@ -368,7 +348,7 @@ App.Views.AuthorPost = Parse.View.extend ({
     //clears our element
     this.$el.empty();
 
-      this.collection.each(function (s) {
+      _.each(this.collection, function (s) {
 
         console.log(s);
 
@@ -742,7 +722,7 @@ $( document ).ready(function(){
       var author = new Parse.Query(App.Models.Post),
           c;
 
-      author.equalTo('user', App.user.attributes.username);
+      author.equalTo('user', user);
 
       author.find({
 
